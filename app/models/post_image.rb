1947ad3744037b68dc9230a297c6ee9d8f5c3479
takuitose <132122425+takuitose@users.noreply.github.com>
2023-05-14 04:47:23 +0000

    [Update]meshiterro(5-21)

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,10 +1,11 @@
 class PostImage < ApplicationRecord
-
+  has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_one_attached :image
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def get_image
     unless image.attached?
@@ -13,7 +14,7 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
